9f1687fc8b01cf6a7f658ae7eb2ad842d0d9216e
dalamcd <dalamcd@gmail.com>
2020-10-21 10:15:47 -0500

    Added dropdown for officer and functionality to filter on arresting officer

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
-import { getConvictions } from "./convictions/ConvictionProvider.js";
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { CriminalList } from "./criminals/CriminalList.js";
+import { OfficerList } from "./officers/OfficerList.js";
+import { OfficerSelect } from "./officers/OfficerSelect.js";
 
 CriminalList();
 ConvictionSelect();
+OfficerList();
+OfficerSelect();
---
+++ scripts/officers/OfficerSelect.js
@@ -0,0 +1,39 @@
+import { getOfficers, useOfficers } from "./OfficerProvider.js"
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".filters__officer");
+eventHub.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id === "officerSelect") {
+        // Get the name of the selected officer
+        const selectedOfficer = changeEvent.target.value
+        // Define a custom event
+        const customEvent = new CustomEvent("officerSelected", {
+            detail: {
+                officer: selectedOfficer
+            }
+        })
+
+        // Dispatch event to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+export const OfficerSelect = () => {
+    getOfficers().then( () => {
+        render(useOfficers());
+    });
+}
+
+const render = officerCollection => {
+    /*
+        Use interpolation here to invoke the map() method on
+        the convictionsCollection to generate the option elements.
+        Look back at the example provided above.
+    */
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="officerSelect">
+            <option value="0">Please select an officer...</option>
+            ${officerCollection.map(offObj => `<option value="${offObj.name}">${offObj.name}</option>`)}
+        </select>
+    `
+}
